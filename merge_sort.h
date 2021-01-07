1823e0c5e5dad9faf0cd58717735218efe382bc0
Jaimee Yeung <68402647+jaimeeyeung@users.noreply.github.com>
2021-01-06 22:31:17 -0500

    Create merge_sort.h

---
+++ merge_sort.h
@@ -0,0 +1,6 @@
+// merge_sort(arr, arr_len) sorts arr in ascending order.
+// requires: arr is not NULL
+//           arr is of length arr_len [not asserted]
+// effects:  might modify arr
+// time: O(n log n)
+void merge_sort(int arr[], int arr_len);
